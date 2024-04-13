5688e62391b4d82c0c89ed3684ddfadc033078c4
Oleksandr-Matsenko <olexandr282@gmail.com>
2024-04-13 17:51:02 +0300

    Task 8.4
    - Showed all information(songs) in PlayListMoveDeleteViewController by genre
    - Added title-header to sections named by genre
    - Little bit customised tableView

--- Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreModel.swift
+++ Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreModel.swift
@@ -8,11 +8,41 @@
 import Foundation
 
 protocol PlaylistByGenreModelDelegate: AnyObject {
+    func dataDidLoad()
     
 }
 
 class PlaylistByGenreModel {
     
     weak var delegate: PlaylistByGenreModelDelegate?
-    private let dataLoader = DataLoaderService()
-}
+       let dataLoader = DataLoaderService()
+       var sections: [Section] = []
+       
+    func loadData(genres: [String]) {
+        dataLoader.loadPlaylist { playlist in
+               if let songs = playlist?.songs {
+                   
+                   let groupedSongs = Dictionary(grouping: songs) { $0.genre }
+                 
+                   let songSections: [Section] = groupedSongs.compactMap { key, value in
+                       return Section(title: key, rows: value)
+                   }
+                   self.sections = songSections
+                   self.delegate?.dataDidLoad()
+               }
+               
+              
+           }
+       }
+   }
+
+   class Section {
+       var title: String
+       var rows: [Song]
+       
+       init(title: String, rows: [Song]) {
+           self.title = title
+           self.rows = rows
+       }
+   }
+
--- Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreViewController.swift
+++ Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreViewController.swift
@@ -7,18 +7,72 @@
 
 import UIKit
 
-class PlaylistByGenreViewController: UIViewController {
+final class PlaylistByGenreViewController: UIViewController {
     
     @IBOutlet weak var contentView: PlaylistByGenreView!
     var model: PlaylistByGenreModel!
+    var playlistByGenre: [String: [Song]] = [:]
+    var genres: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupInitialState()
+        
     }
     
     private func setupInitialState() {
-        
         model = PlaylistByGenreModel()
+        model.delegate = self
+        contentView.tableView.dataSource = self
+        contentView.tableView.delegate = self
+        model.loadData(genres: genres)
+        customizeTableView()
+    }
+    private func customizeTableView() {
+        contentView.tableView.separatorColor = .systemBlue
+        contentView.tableView.separatorInset = .init(top: 0, left: 15, bottom: 0, right: 15)
+    }
+}
+
+extension PlaylistByGenreViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+        return "\(genres[section])".uppercased()
+    }
+}
+
+extension PlaylistByGenreViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let genre = genres[section]
+        return playlistByGenre[genre]?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = contentView.tableView.dequeueReusableCell(withIdentifier: "MainPlaylistCell") else {
+            assertionFailure()
+            return UITableViewCell()
+        }
+        let genre = genres[indexPath.section]
+        if let songsInGenre = playlistByGenre[genre] {
+            let song = songsInGenre[indexPath.row]
+            cell.textLabel?.text = "Track: \(song.songTitle)"
+            cell.textLabel?.textColor = .systemBlue
+            cell.textLabel?.font = .systemFont(ofSize: 15, weight: .bold)
+            cell.detailTextLabel?.text = "Album: \(song.albumTitle), \nAuthor: \(song.author), \nGenre: \(song.genre)"
+            cell.detailTextLabel?.font = .systemFont(ofSize: 13, weight:.regular)
+            cell.detailTextLabel?.numberOfLines = 0
+        }
+        return cell
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return genres.count
+    }
+}
+
+extension PlaylistByGenreViewController: PlaylistByGenreModelDelegate {
+    func dataDidLoad() {
+        contentView.tableView.reloadData()
+        genres = model.sections.map{$0.title}
+        playlistByGenre = Dictionary(uniqueKeysWithValues: model.sections.map{($0.title, $0.rows)})
     }
 }
